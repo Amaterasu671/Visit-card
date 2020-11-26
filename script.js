7612acaebcd6d165b73f2d939ad2c4db8965cfac
Amaterasu671 <74874600+Amaterasu671@users.noreply.github.com>
2020-11-26 07:21:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,34 @@
 var nbClickVerso = 0;
 var nbClickRecto = 0;
+
 function agrandir(elt){
   var elem = document.getElementById(elt);
   if(elem === document.getElementById('verso')){
     nbClickVerso++;
-    if(nbClickVerso%2 != 0){
-      elem.style.transform = "scale(2, 2)";
-      elem.style.top = "0px";
-      elem.style.left = "0px";
-      elem.style.right = "0px";
-      elem.style.bottom = "0px";
-      elem.style.position = "absolute";
-      elem.style.zIndex = 1;
-    }
+    if(nbClickVerso%2 != 0)
+      RemettrePlace('0%', '0%', '0%', '0%', 'absolute', 1,'scale(2, 2)', elem);
     else if (nbClickVerso%2 == 0) {
       elem.style.transform = "scale(1, 1)";
-      elem.style.top = "0%";
-      elem.style.left = "30%";
-      elem.style.right = "0";
-      elem.style.bottom = "0";
-      elem.style.position = "absolute";
-      elem.style.zIndex = 0;
+      RemettrePlace('0%', '30%', '0%', '0%', 'absolute', 0,'scale(1, 1)', elem);
     }
   }
   else if(elem == document.getElementById('recto')){
     nbClickRecto++;
-    if(nbClickRecto%2 != 0){
-      elem.style.transform = "scale(2, 2)";
-      elem.style.top = "0px";
-      elem.style.left = "0px";
-      elem.style.right = "0px";
-      elem.style.bottom = "0px";
-      elem.style.position = "absolute";
-      elem.style.zIndex = 1;
-    }
+    if(nbClickRecto%2 != 0)
+      RemettrePlace('0%', '0%', '0%', '0%', 'absolute', 1,'scale(2, 2)', elem);
     else if (nbClickRecto%2 == 0) {
-      document.getElementById('verso').style.visibility = "visible";
       elem.style.transform = "scale(1, 1)";
-      elem.style.top = "20%";
-      elem.style.left = "-30%";
-      elem.style.right = "0";
-      elem.style.bottom = "0";
-      elem.style.position = "absolute";
-      elem.style.zIndex = 0;
+      RemettrePlace('20%', '-30%', '0%', '0%', 'absolute', 0, 'scale(1, 1)', elem);
     }
   }
+}
+
+function RemettrePlace(top, left, right, bottom, position, zIndex,transform, element){
+  element.style.transform = transform;
+  element.style.top = top;
+  element.style.left = left;
+  element.style.right = right;
+  element.style.bottom = bottom;
+  element.style.position = position;
+  element.style.zIndex = zIndex;
 }
